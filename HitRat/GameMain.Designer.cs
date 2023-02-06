0eb6cb182ede12904a58de36949e1be748c1e7d4
hades <hades.wang@twchia.com>
2023-02-06 16:24:13 +0800

    Add CountDownTimer when game is starting

--- HitRat/GameMain.Designer.cs
+++ HitRat/GameMain.Designer.cs
@@ -28,38 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.textBoxCountDownTimer = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.buttonStartGame = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxCountDownTimer
             // 
-            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox1.Location = new System.Drawing.Point(169, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(176, 31);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "倒數計時:60秒";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 38);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "開始遊戲";
-            this.button1.UseVisualStyleBackColor = true;
+            this.textBoxCountDownTimer.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxCountDownTimer.Location = new System.Drawing.Point(197, 19);
+            this.textBoxCountDownTimer.Name = "textBoxCountDownTimer";
+            this.textBoxCountDownTimer.Size = new System.Drawing.Size(176, 31);
+            this.textBoxCountDownTimer.TabIndex = 0;
+            this.textBoxCountDownTimer.Text = "倒數計時:60秒";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox2.Location = new System.Drawing.Point(612, 12);
+            this.textBox2.Location = new System.Drawing.Point(612, 21);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(176, 31);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "得分:0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // buttonStartGame
+            // 
+            this.buttonStartGame.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonStartGame.Location = new System.Drawing.Point(12, 14);
+            this.buttonStartGame.Name = "buttonStartGame";
+            this.buttonStartGame.Size = new System.Drawing.Size(156, 38);
+            this.buttonStartGame.TabIndex = 3;
+            this.buttonStartGame.Text = "開始遊戲";
+            this.buttonStartGame.UseVisualStyleBackColor = true;
+            this.buttonStartGame.Click += new System.EventHandler(this.buttonStartGame_Click_1);
             // 
             // FormGameMain
             // 
@@ -68,11 +75,12 @@
             this.BackgroundImage = global::HitRat.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStartGame);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxCountDownTimer);
             this.Name = "FormGameMain";
             this.Text = "打地鼠";
+            this.Load += new System.EventHandler(this.FormGameMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,9 +88,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxCountDownTimer;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button buttonStartGame;
     }
 }
 
--- HitRat/GameMain.cs
+++ HitRat/GameMain.cs
@@ -12,9 +12,48 @@ namespace HitRat
 {
     public partial class FormGameMain : Form
     {
+        const int GAME_ROUND_TIME = 60;
+        const int GAME_INTERVAL = 100;
+
+        int _gameTimer = 0;
+
         public FormGameMain()
         {
             InitializeComponent();
+        }
+
+        private void FormGameMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartGame()
+        {
+            _gameTimer = GAME_ROUND_TIME;
+            gameTimer.Interval = GAME_INTERVAL;
+            gameTimer.Enabled = true;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            textBoxCountDownTimer.Text = $"倒數計時:{_gameTimer}秒";
+            _gameTimer--;
+
+            IfNeedToEndGame();
+        }
+
+        private void IfNeedToEndGame()
+        {
+            if (_gameTimer == -1)
+            {
+                gameTimer.Enabled = false;
+                textBoxCountDownTimer.Text = $"遊戲結束";
+            }
+        }
+
+        private void buttonStartGame_Click_1(object sender, EventArgs e)
+        {
+            StartGame();
         }
     }
 }
